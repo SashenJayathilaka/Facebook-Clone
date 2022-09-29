7f3b8c81810c6e8a6987165a8f01aba80881862c
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-09-29 10:26:20 +0530

    🔥 added facebook authentication

--- components/SignInWithFacebook.tsx
+++ components/SignInWithFacebook.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LockPersonIcon from "@mui/icons-material/LockPerson";
 import { useSignInWithFacebook } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
+import toast, { Toaster } from "react-hot-toast";
 
 type SignInWithFacebookProps = {};
 
@@ -9,8 +10,32 @@ const SignInWithFacebook: React.FC<SignInWithFacebookProps> = () => {
   const [signInWithFacebook, user, loading, error] =
     useSignInWithFacebook(auth);
 
+  const errorMessage = () => {
+    if (error?.name) {
+      toast.error(
+        "As you already have a Google account for the same email, log in with your google account",
+        {
+          duration: 8000,
+          style: {
+            background: "#fff",
+            color: "#015871",
+            fontWeight: "bolder",
+            fontSize: "15px",
+            padding: "5px",
+            textAlign: "center",
+          },
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    errorMessage();
+  }, [error]);
+
   return (
     <>
+      <Toaster />
       <button
         onClick={() => signInWithFacebook()}
         className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-xl font-medium rounded-md text-white bg-blue-600 hover:bg-blue-300 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-50"
