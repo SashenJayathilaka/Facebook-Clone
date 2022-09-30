77a8814b0b7bcb44ed90ac89e10ffdcc5359ea06
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-09-30 09:35:24 +0530

    ## update Skeleton

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,11 +2,13 @@ import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { firestore } from "../firebase/firebase";
 import Post from "./Post";
+import PostSkeleton from "./Skeleton";
 
 type Props = {};
 
 export default function Feed({}: Props) {
   const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(
     () =>
@@ -19,20 +21,40 @@ export default function Feed({}: Props) {
     [firestore]
   );
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (posts) {
+        setLoading(false);
+      } else {
+        setLoading(true);
+      }
+    }, 6000);
+  }, [posts]);
+
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <Post
-            id={post.id}
-            author={post.data().username}
-            caption={post.data().caption}
-            image={post.data().image}
-            profileImage={post.data().profileImage}
-            timestamp={post.data().timestamp}
-          />
+    <>
+      {loading ? (
+        <>
+          {posts.map((data) => (
+            <PostSkeleton key={data.id} loading={loading} />
+          ))}
+        </>
+      ) : (
+        <div>
+          {posts.map((post) => (
+            <div key={post.id}>
+              <Post
+                id={post.id}
+                author={post.data().username}
+                caption={post.data().caption}
+                image={post.data().image}
+                profileImage={post.data().profileImage}
+                timestamp={post.data().timestamp}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
