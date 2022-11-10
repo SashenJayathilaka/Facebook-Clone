e2790ce08966205b9d1323d49ad1ffda415afc38
Sashen Jayathilaka <99184393+SashenJayathilaka@users.noreply.github.com>
2022-11-10 09:56:46 +0530

    🆕 Features(#30)

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,7 @@
 import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 import { firestore } from "../firebase/firebase";
 import Post from "./Post";
 import PostSkeleton from "./Skeleton";
@@ -25,22 +27,35 @@ export default function Feed({}: Props) {
     setTimeout(() => {
       if (posts) {
         setLoading(false);
-      } else {
-        setLoading(true);
-      }
-    }, 6000);
+      } else return;
+    }, 3000);
   }, [posts]);
 
   return (
     <>
       {loading ? (
-        <>
-          {posts.map((data) => (
-            <PostSkeleton key={data.id} loading={loading} />
-          ))}
-        </>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+          <PostSkeleton loading={loading} />
+        </motion.div>
       ) : (
-        <div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
           {posts.map((post) => (
             <div key={post.id}>
               <Post
@@ -54,7 +69,7 @@ export default function Feed({}: Props) {
               />
             </div>
           ))}
-        </div>
+        </motion.div>
       )}
     </>
   );
